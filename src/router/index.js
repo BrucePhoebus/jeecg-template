bc07f925726378f61ae7371faeba0be208033a22
phoebus <1634372267@qq.com>
2020-02-28 16:15:24 +0800

    基本完成不需要的功能改造，下一步改造布局和全路由由全动态变为部分动态

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import { constantRouterMap } from '@/config/router.config'
 
-//update-begin-author:taoyan date:20191011 for:TASK #3214 【优化】访问online功能测试 浏览器控制台抛出异常
 try {
   const originalPush = Router.prototype.push
   Router.prototype.push = function push(location) {
@@ -10,13 +9,12 @@ try {
   }
 } catch (e) {
 }
-//update-end-author:taoyan date:20191011 for:TASK #3214 【优化】访问online功能测试 浏览器控制台抛出异常
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: process.env.VUE_APP_BASE_URL,
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRouterMap
 })
